efc88c7b327a01fe9181b2c43c54dca7add300dd
sarbell <96sarahbell@gmail.com>
2021-09-27 03:04:02 -0600

    changed server

--- server.js
+++ server.js
@@ -61,11 +61,3 @@ app.get('*', (req, res) => {
   const fullpath = path.join(__dirname, 'client', 'build', 'index.html')
   res.sendFile(fullpath);
 });
-
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
